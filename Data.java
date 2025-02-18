f5815375ac42acdcaaae1af44fddd7878a30a10e
Niwhar Amin <145506476+ninoa2000@users.noreply.github.com>
2025-02-18 23:15:04 +0000

    code

--- Data.java
+++
@@ -1,9 +0,0 @@
-public class Data {
-    public void storeData(String data) {
-        System.out.println("Data stored: " + data);
-    }
-
-    public void retrieveData() {
-        System.out.println("Retrieving stored data...");
-    }
-}
